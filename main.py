ecab87672ea8b54e51b59f5160eebb9153de951d
westcoastcoder <wwscottbc@gmail.com>
2020-10-15 16:52:28 -0700

    Added command processing loop and try/except block to catch user input errors

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import sys
-print(sys.argv)
+# print(sys.argv)
 
-for i, item in enumerate(sys. argv):
-    print("{0} {1}".format(i, item))
+# for i, item in enumerate(sys. argv):
+#    print("{0} {1}".format(i, item))
+
+args = sys.argv
+print(args)
+
+try:
+    command = args[1]
+except IndexError:
+    print("Invalid arguments!")
+    sys.exit(1)
+
+if command not in ("add", "remove", "list"):
+    print("Invalid command, Use add/remove/list")
+
+if command == "add":
+    print("adding")
+elif command == "remove":
+    print("removing")
+elif command == "list":
+    print("listing")
+else:
+    print("invalid command!")
