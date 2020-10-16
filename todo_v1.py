8aa6ecd7956698f3a074c7434a7f18bcfe49cc64
westcoastcoder <wwscottbc@gmail.com>
2020-10-15 23:08:28 -0700

    v1 of task program.

---
+++ todo_v1.py
@@ -0,0 +1,55 @@
+import sys
+
+args = sys.argv
+
+tasks = []
+
+try:
+    command = args[1]
+except IndexError:
+    print("Invalid arguments!")
+    sys.exit(1)
+
+if command not in ("add", "remove", "list"):
+    print("Invalid command, Use add/remove/list")
+    sys.exit(1)
+
+if command == "add":
+    title = args[2]
+    content = args[3]
+    task = title + "|" + content
+    file = open("tasks.txt", "a")
+    file.write(task+"\n")
+    file.close()
+elif command == "remove":
+    try:
+        file = open("tasks.txt", 'r')
+    except IOError as e:
+        print(str(e))
+        sys.exit(1)
+    file.close()
+    tasks = file.readlines()
+    tasks = [task.strip() for task in tasks]
+    task_id = args[2]
+    del tasks[int(task_id)]
+    file = open("tasks.txt", "w")
+    tasks = [task + "\n" for task in tasks]
+    file.writelines(tasks)
+elif command == "list":
+    try:
+        file = open("tasks.txt", "r")
+    except IOError as e:
+        print(str(e))
+        sys.exit(1)
+    tasks = file.readlines()
+    if len(tasks) == 0:
+        print("No tasks present.")
+    else:
+        print("|-{0}---{1}------{2}----|".format("index", "title", "content"))
+        tasks = [task.strip() for task in tasks]
+        for i in range(len(tasks)):
+            title, content = tasks[i].split("|")
+            print("|-{0}--{1}----{2}-|".format(i, title, content))
+    file.close()
+else:
+    print("invalid command!")
